eec627439d0318ad3f8ceabed38444d14773ec80
Fatih K <fatihku@gmail.com>
2026-04-19 11:38:04 +0300

    Initial Commit

---
+++ YTDL Manager/YTDL_ManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  YTDL_ManagerApp.swift
+//  YTDL Manager
+//
+//  Created by Fatih Kuyucuoglu on 19.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct YTDL_ManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
